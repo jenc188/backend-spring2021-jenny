36cec8dfe798eafa69901db88973d6221383effa
Jenny C <jennc388@gmail.com>
2021-04-07 21:08:45 -0700

    modified index file

--- assignments/week3/exampleserver/index.js
+++ assignments/week3/exampleserver/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+// const bodyParser = require("body-parser"); DEPRECATED
 
 //"Run" the Express package.
 const app = express();
 
 // Allows our Express server to understand HTTP requests.
 const http = require("http").Server(app);
+
+// Use body-parser to convert our POST data to proper JS datatypes. CALL METHODS FROM EXPRESS PACKAGE INSTEAD.
+app.use(express.json());
+app.use(express.urlencoded(
+    {extended: false}
+    ));
 
 const port = 3000; // Most common "development" port is 8080.
 
@@ -22,6 +29,10 @@ app.use("/funnywebsite", express.static("public_html/funny/"));
 //POST routes
 
 app.post("/submitNumber", function(request, response) {
+
+    let dataFromFrontEnd = request.body;
+
+    console.log("Our visitor says: " + dataFromFrontEnd.message);
 
     response.send("Thank you for our request.");
 });

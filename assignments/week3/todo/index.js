449317a5955b676154d5d6284c576554a34689c8
Jenny C <jennc388@gmail.com>
2021-04-18 21:33:05 -0700

    continue to work on ToDo app

--- assignments/week3/todo/index.js
+++ assignments/week3/todo/index.js
@@ -12,21 +12,24 @@ http.listen(port);
 
 console.log("Express server is now running on port "+ port);
 
+// Variables for the task object, and filename.
 let tasks;
 let taskFileName = "tasks.json";
 
 // Prepare JSON tasks file
 if (fs.existsSync(taskFileName)) {
+    // Read file
     let fileContents = fs.readFileSync(taskFileName, "utf-8");
-    task = JSON.parse(fileContents);
+    // Convert JSON to actual JavaScript Objects.
+    tasks = JSON.parse(fileContents);
+    // TODO: Write code that converts generic Objects into Task Objects.
 } else {
+    // Otherwise, if file does not exist, create task Object...
     tasks = {
         incompleted: [],
-        completed: [],
-        deleted: []
     }
-
-    fs.writeFileSync(taskFileName, JSON.stringify(tasks), "utf-8");
+ // ...Write task object to filename.
+fs.writeFileSync(taskFileName, JSON.stringify(tasks), "utf-8");
 }
 
 // Body Parser
@@ -34,4 +37,70 @@ app.use(express.json({strict: false}));
 app.use(express.urlencoded({extended: false}));
 
 // Routes
+
+// The default route for when a visitor requests the URL without a file path. 
 app.use("/", express.static("public_html/"));
+
+// POST Handler for adding a new task.
+app.post("/add-task", function (req, res) {
+    let taskData = req.body;
+    // TODO: detect if there is actual text in text property.
+   // Create a Task Object based on the data recieved from the front-end.
+    let taskObject = new task.Task(taskData.text, taskData.priority, taskData.dueDate);
+
+    // Store new Task Object into the tasks incompleted array.
+    task.incompleted.push(taskObject);
+
+    // "Update" the json file.
+    saveFile();
+
+     // Send a response to the front-end.
+    res.send({error: null});
+});
+
+// POST Handler for getting all tasks.
+app.post("/get-tasks", function (req, res) {
+
+    // Filter out the tasks that have been completed or deleted.
+    let incompleteArray = tasks.incompleted.filter(function(task) {
+        console.log(task);
+        // If the task has a deleted or completed date, it fails the filter test.
+        if (task.isDeleted() || task.isCompleted()) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+
+    // Build an object holding all the Task objects that passed the filter test.
+    let responseObject = {
+        incompleted: incompleteArray
+    };
+     // Send the resulting object back to the front-end.
+    res.send(responseObject);
+});
+
+// POST Handler for deleting a single task.
+app.post("/delete-task", function (req, res) {
+    let id = req.body.id;
+
+    // Go through each task in the tasks array and find the one with the matching ID.
+    for (let i = 0; i < tasks.incompleted.length; i++) {
+        if (tasks.incompleted[i].id === id) {
+            //if ID matches them mark the Task Object deleted.
+            tasks.incompleted[i].markDeleted();
+            break;
+        }
+    }
+
+    // Just send a message to the front-end.
+    res.send({});
+});
+
+// TODO: Find out a way to delay multiple calls of this function.
+
+// the saveFile function will convert our task Object into JSON and save it to filename.
+function saveFile() {
+    let json = JSON.stringify(tasks);
+    fs.writeFileSync(taskFileName, json, "utf-8");
+}
--- assignments/week3/todo/public_html/scripts/main.js
+++ assignments/week3/todo/public_html/scripts/main.js
@@ -1,3 +1,52 @@
+let base_URL = "http://localhost:3000";
+
 $(function () {
-    console.log("hello");
+    refreshTaskList();
+
+    $("#add-task").click(function () {
+        let taskObject = {
+            text: $("#task-text").val(),
+            priority: $("new-task div input[type=radio]:checked").val(),
+            dueDate: $("#due-date").val()
+        };
+
+        console.log(taskObject);
+        
+        $.post(base_URL + "/add-task", taskObject, function (data) {
+            refreshTaskList();
+        });
+    });
 });
+    
+//function to refresh and load 
+function refreshTaskList() {
+    $("div#tasks").empty();
+
+    $.post(base_URL + "/get-tasks", {}, function(data) {
+        let tasks = data.incompleted;
+
+        tasks.forEach(function (task) {
+            let html = `
+<div class="task" data-id=${task.id}>
+    <button><i class="fas fa-check"></i></button>
+    <p>${task.text}</p><p>${task.dueDate}</p>
+    <button class="deleted><i class="fas fa-trash-alt"></i></button>
+    <button><i class="far fa-edit"></i></button>
+</div>`;
+        
+            $("div#tasks").append(html);
+        });
+
+        $("button.delete").click(function (){
+
+            let deleteObject = {
+                id: $(this).parent().attr("data-id")
+            }
+
+            $.post("/delete-task", deleteObject, function (data) {
+                $(this).parent().remove();
+            });
+        });
+    });
+}
+    

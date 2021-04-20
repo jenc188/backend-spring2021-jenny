262f69461f010e56af0b1196c4c7ff7215791285
Jenny C <jennc388@gmail.com>
2021-04-19 19:16:06 -0700

    work on todo app

--- assignments/week3/todo/index.js
+++ assignments/week3/todo/index.js
@@ -1,16 +1,16 @@
+//backend 
+// DO NOT USE "name" as a variable name when using Node.
+
 const express = require("express");
 const fs = require("fs");
 const task = require("./Task.js");
-
 const app = express();
-
 const http = require("http").Server(app);
-
 const port = 3000;
 
 http.listen(port);
 
-console.log("Express server is now running on port "+ port);
+console.log("Express server is now running on port " + port);
 
 // Variables for the task object, and filename.
 let tasks;
@@ -18,19 +18,31 @@ let taskFileName = "tasks.json";
 
 // Prepare JSON tasks file
 if (fs.existsSync(taskFileName)) {
-    // Read file
-    let fileContents = fs.readFileSync(taskFileName, "utf-8");
-    // Convert JSON to actual JavaScript Objects.
-    tasks = JSON.parse(fileContents);
-    // TODO: Write code that converts generic Objects into Task Objects.
+   // Read file
+   let fileContents = fs.readFileSync(taskFileName, "utf-8");
+   // Convert JSON to actual JavaScript Objects.
+   tasks = JSON.parse(fileContents);
+   // TODO: Write code that converts generic Objects into Task Objects.
+
+   // Convert the JS object from JSON to proper Task Objects.
+      convertedObjects = [];
+
+      tasks.incompleted.forEach(function (jsonTask) {
+         let newTaskObject = new task.Task();
+         convertedObjects.push(newTaskObject.jsonConvert(jsonTask));
+      });
+
+      tasks.incompleted = convertedObjects;
 } else {
-    // Otherwise, if file does not exist, create task Object...
-    tasks = {
-        incompleted: [],
-    }
- // ...Write task object to filename.
-fs.writeFileSync(taskFileName, JSON.stringify(tasks), "utf-8");
+   // Otherwise, if file does not exist, create task Object...
+   tasks = {
+      incompleted: [],
+   }
+   // ...Write task object to filename.
+   fs.writeFileSync(taskFileName, JSON.stringify(tasks), "utf-8");
 }
+
+console.log(tasks.incompleted);
 
 // Body Parser
 app.use(express.json({strict: false}));
@@ -43,64 +55,67 @@ app.use("/", express.static("public_html/"));
 
 // POST Handler for adding a new task.
 app.post("/add-task", function (req, res) {
-    let taskData = req.body;
-    // TODO: detect if there is actual text in text property.
+   let taskData = req.body;
+   // TODO: detect if there is actual text in text property.
    // Create a Task Object based on the data recieved from the front-end.
-    let taskObject = new task.Task(taskData.text, taskData.priority, taskData.dueDate);
+   let taskObject = new task.Task(taskData.text, taskData.priority, taskData.dueDate);
 
-    // Store new Task Object into the tasks incompleted array.
-    task.incompleted.push(taskObject);
+   // Store new Task Object into the tasks incompleted array.
+   tasks.incompleted.push(taskObject);
 
-    // "Update" the json file.
-    saveFile();
+   // "Update" the json file.
+   saveFile();
 
-     // Send a response to the front-end.
-    res.send({error: null});
+   // Send a response to the front-end.
+   res.send({error: null});
 });
 
 // POST Handler for getting all tasks.
 app.post("/get-tasks", function (req, res) {
+   
+   // Filter out the tasks that have been completed or deleted.
+   let incompleteArray = tasks.incompleted.filter(function (task) {
+      console.log(task);
+      // If the task has a deleted or completed date, it fails the filter test.
+      if (task.isDeleted() || task.isCompleted()) {
+         return false;
+      } else {
+         return true;
+      }
+   });
 
-    // Filter out the tasks that have been completed or deleted.
-    let incompleteArray = tasks.incompleted.filter(function(task) {
-        console.log(task);
-        // If the task has a deleted or completed date, it fails the filter test.
-        if (task.isDeleted() || task.isCompleted()) {
-            return false;
-        } else {
-            return true;
-        }
-    });
+   // Build an object holding all the Task objects that passed the filter test.
+   let responseObject = {
+      incompleted: incompleteArray
+   };
 
-    // Build an object holding all the Task objects that passed the filter test.
-    let responseObject = {
-        incompleted: incompleteArray
-    };
-     // Send the resulting object back to the front-end.
-    res.send(responseObject);
+   // Send the resulting object back to the front-end.
+   res.send(responseObject);
 });
 
 // POST Handler for deleting a single task.
 app.post("/delete-task", function (req, res) {
-    let id = req.body.id;
 
-    // Go through each task in the tasks array and find the one with the matching ID.
-    for (let i = 0; i < tasks.incompleted.length; i++) {
-        if (tasks.incompleted[i].id === id) {
-            //if ID matches them mark the Task Object deleted.
-            tasks.incompleted[i].markDeleted();
-            break;
-        }
-    }
+   let id = req.body.id;
 
-    // Just send a message to the front-end.
-    res.send({});
+   // Go through each task in the tasks array and find the one with the matching ID.
+   for (let i = 0; i < tasks.incompleted.length; i++) {
+      if (tasks.incompleted[i].id === id) {
+         // If ID matches them mark the Task Object deleted.
+         tasks.incompleted[i].markDeleted();
+         break;
+      }
+   }
+
+   // Just send a message to the front-end.
+   res.send({});
 });
+
 
 // TODO: Find out a way to delay multiple calls of this function.
 
 // the saveFile function will convert our task Object into JSON and save it to filename.
 function saveFile() {
-    let json = JSON.stringify(tasks);
-    fs.writeFileSync(taskFileName, json, "utf-8");
+   let json = JSON.stringify(tasks);
+   fs.writeFileSync(taskFileName, json, "utf-8");
 }
--- assignments/week3/todo/public_html/scripts/main.js
+++ assignments/week3/todo/public_html/scripts/main.js
@@ -1,3 +1,4 @@
+//frontend js
 let base_URL = "http://localhost:3000";
 
 $(function () {
@@ -6,12 +7,12 @@ $(function () {
     $("#add-task").click(function () {
         let taskObject = {
             text: $("#task-text").val(),
-            priority: $("new-task div input[type=radio]:checked").val(),
+            priority: $("#new-task div input[type=radio]:checked").val(),
             dueDate: $("#due-date").val()
         };
 
         console.log(taskObject);
-        
+
         $.post(base_URL + "/add-task", taskObject, function (data) {
             refreshTaskList();
         });
@@ -30,23 +31,45 @@ function refreshTaskList() {
 <div class="task" data-id=${task.id}>
     <button><i class="fas fa-check"></i></button>
     <p>${task.text}</p><p>${task.dueDate}</p>
-    <button class="deleted><i class="fas fa-trash-alt"></i></button>
-    <button><i class="far fa-edit"></i></button>
+    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    <button class="edit"><i class="far fa-edit"></i></button>
 </div>`;
-        
+
             $("div#tasks").append(html);
         });
 
-        $("button.delete").click(function (){
+        $("button.delete").click(function () {
+
+            let button = $(this);
 
             let deleteObject = {
-                id: $(this).parent().attr("data-id")
+                id: button.parent().attr("data-id")
             }
 
             $.post("/delete-task", deleteObject, function (data) {
-                $(this).parent().remove();
+                console.log("responded!");
+                button.parent().remove();
             });
+        });
+
+        $("button.edit").click(function () {
+            let htmlDrawer = `
+<div class="edit-panel">
+    <label for="task-text">Task</label>
+    <input type="text" id="task-text" />
+    <br />
+    <label for="priority-1">Priority 1</label>
+    <input type="radio" name="priority" id="priority-1" />
+    <label for="priority-2">Priority 2</label>
+    <input type="radio" name="priority" id="priority-2" />
+    <label for="priority-3">Priority 3</label>
+    <input type="radio" name="priority" id="priority-3" />
+    <br />
+    <label for="due-date">Due Date</label>
+    <input type="date" id="due-date" />
+</div>`;
+
+            $(this).parent().append(htmlDrawer);
         });
     });
 }
-    

522742c3f9b7085bed795db3aabfbb3eb625a529
Jenny C <jennc388@gmail.com>
2021-05-03 16:24:52 -0700

    work on dataset file

--- assignments/week5/dataset/dataset.js
+++ assignments/week5/dataset/dataset.js
@@ -1,34 +1,36 @@
+const {performance} = require('perf_hooks');
+let t0 = performance.now();
+
 const fs = require("fs");
 
 let dataset = fs.readFileSync("dataset.csv", "utf-8");
 
 let lineArray = dataset.split("\n");
-
 let dataHeadings = lineArray[0].split(",");
-
-
 let ignoredIndexes = [0,5,6,7,8,9,10,11,12,13,19,20,23,24,25,26];
+let objectArray = [];
 
-console.log(convertEntry(lineArray[5002]));
+for (let i = 0; i < lineArray.length; i++) {
+    objectArray.push(convertEntry(lineArray[i], dataHeadings));
+}
 
+// console.log(convertEntry(lineArray[5002]));
 
-
-
-
-
-
-
-
+console.log(objectArray);
 
 
 
 
-function convertEntry(entry) {
 
-    let dataArray = entry.split(",");
+
+
+
+function convertEntry(entry, titles) {
+
+    let dataArray = quoteFix(entry);
     let dataObject = {};
 
-    dataHeadings.forEach(function (arrayElement, i){
+    titles.forEach(function (arrayElement, i){
 
         if (ignoredIndexes.includes(i)){
             return; 
@@ -40,6 +42,7 @@ function convertEntry(entry) {
     return dataObject;
 }
 
+// Issues with array tracking, break down further to understand EXACT logic
 function quoteFix(entry) {
 
     let regex = /"/m;
@@ -48,20 +51,55 @@ function quoteFix(entry) {
     if (results) {
         let entryArray = entry.split(",");
         let entryWithoutQuotes = [];
+        let closingQuoteIndex = -1;
 
         for (let i = 0; i < entryArray.length; i++) {
-            if (entryArray[i][0] === '"'){
-                
-                // add the following array elements until closing quote into a string. Push the complete string into array.
 
-               
+            // adding continue and not adding 1, starts at "motorcycle" for 5002
+            if (i < closingQuoteIndex) {
+                i = closingQuoteIndex + 1;
+            }
+
+            let value = entryArray[i];
+                
+            if (value[0] === '"') {
+                    
+                let completeString = value + ",";
+
+                for (let j = i + 1; j < entryArray.length; j++) {
+                        
+                    let endingValue = entryArray[j];
+
+                    if (endingValue[endingValue.length -1] === '"') {
+                        // completeString += endingValue;
+                        completeString = completeString + endingValue;
+                        closingQuoteIndex = j;
+                        break;
+                    } else {
+                        completeString = completeString + endingValue + ",";
+                    }
+
+                }
+                entryWithoutQuotes.push(removeQuotes(completeString));
             } else {
-                entryWithoutQuotes.push(entryArray[i]);
+                entryWithoutQuotes.push(value);
             }
         }
 
+        return entryWithoutQuotes;
     } else {
-        return entry;
+        return entry.split(",");
     }
 }
+
+function removeQuotes(text) {
+    let textArray = text.split("");
+    textArray.pop();
+    textArray.shift();
+    return textArray.join("");
+}
+
+let t1 = performance.now();
+
+console.log(`This script took ${t1 - t0} milliseconds to execute!`);
 
